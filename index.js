7dd505b812f09433fdb67e8ca207af763cbd7da6
JeongHeon Woo <email@wookingwoo.com>
2023-05-25 13:55:35 +0900

    :closed_lock_with_key: Use dotenv

--- index.js
+++ index.js
@@ -16,7 +16,21 @@ const {
   FieldValue,
 } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./data/firebase-IAM-key/namsigdang-crawler-firebase-adminsdk.json");
+require("dotenv").config(); // dotenv 패키지 로드
+
+const serviceAccount = {
+  // 환경 변수를 사용하여 Firebase 서비스 계정 정보 설정
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // 개행 문자 처리
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 initializeApp({
   credential: cert(serviceAccount),
