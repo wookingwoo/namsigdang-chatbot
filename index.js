ca2c898c1e1cf0656a56487512e0a44b9873cfa3
JeongHeon Woo <email@wookingwoo.com>
2023-05-27 00:38:39 +0900

    Change response BasicCard to ListCard

--- index.js
+++ index.js
@@ -295,12 +295,20 @@ apiRouter.post("/menu", async function (req, res) {
 
   // responseSimpleText(
   //   res,
-  //   `'${selectedDate}' 식단 정보 \n\n[아침]\n${selectedBreakfastMenu}\n\n[점심]\n${selectedLunchMenu}\n\n[저녁]\n${selectedDinnerMenu}`
+  //   `<${selectedDate}> 식단 정보 \n\n[아침]\n${selectedBreakfastMenu}\n\n[점심]\n${selectedLunchMenu}\n\n[저녁]\n${selectedDinnerMenu}`
   // );
 
-  responseBasicCard(
+  // responseBasicCard(
+  //   res,
+  //   `<${selectedDate}> 식단 정보 \n\n[아침]\n${selectedBreakfastMenu}\n\n[점심]\n${selectedLunchMenu}\n\n[저녁]\n${selectedDinnerMenu}`
+  // );
+
+  responseListCard(
     res,
-    `'${selectedDate}' 식단 정보 \n\n[아침]\n${selectedBreakfastMenu}\n\n[점심]\n${selectedLunchMenu}\n\n[저녁]\n${selectedDinnerMenu}`
+    selectedDate,
+    selectedBreakfastMenu,
+    selectedLunchMenu,
+    selectedDinnerMenu
   );
 });
 
@@ -343,6 +351,66 @@ function responseBasicCard(res, responseText) {
           basicCard: {
             title: responseText,
             description: responseText,
+
+            buttons: [
+              {
+                label: "공유하기",
+                action: "share",
+              },
+            ],
+
+            quickReplies: [
+              {
+                messageText: "메뉴 보기",
+                action: "message",
+                label: "메뉴 보기",
+              },
+              {
+                messageText: "달력에서 선택하기",
+                action: "message",
+                label: "달력에서 선택하기",
+              },
+            ],
+          },
+        },
+      ],
+    },
+  };
+
+  console.log("responseBody:", responseBody);
+  res.status(200).send(responseBody);
+}
+
+function responseListCard(
+  res,
+  selectedDate,
+  selectedBreakfastMenu,
+  selectedLunchMenu,
+  selectedDinnerMenu
+) {
+  let responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          listCard: {
+            header: {
+              title: `<${selectedDate}> 식단 정보`,
+            },
+            items: [
+              {
+                title: "아침",
+                description: selectedBreakfastMenu,
+              },
+              {
+                title: "점심",
+                description: selectedLunchMenu,
+              },
+              {
+                title: "저녁",
+                description: selectedDinnerMenu,
+              },
+            ],
 
             buttons: [
               {
